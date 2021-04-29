9ed52b7ee0dac75a409009ce2fcdd2fc490bbab8
Yoshihiro Tanaka <contact@cordea.jp>
2021-04-29 16:55:33 +0900

    parse the lcov file

--- lib/flutter_coverage/plugin.rb
+++ lib/flutter_coverage/plugin.rb
@@ -7,12 +7,40 @@ module Danger
     def print
       raise "The coverage file could not be found." unless File.exist?(coverage_path)
 
-      parse_lcov(File.readlines(coverage_path, chomp: true))
+      cov = parse_lcov(File.readlines(coverage_path, chomp: true))
     end
 
     private
 
-    def parse_lcov(lines) end
+    def parse_lcov(lines)
+      cov = []
+      name = ""
+      line_cov = []
+      lf = lh = 0
+
+      lines.each do |line|
+        key, value = line.split(":")
+        case key
+        when "SF"
+          name = value
+        when "DA"
+          n, c = value.split(",")
+          line_cov.push(LineCoverage.new(n.to_i, c.to_i))
+        when "LF"
+          lf = value.to_i
+        when "LH"
+          lh = value.to_i
+        when "end_of_record"
+          cov.push(Coverage.new(name, line_cov, lf, lh))
+          name = ""
+          line_cov = []
+          lf = lh = 0
+        else
+          warn "An unknown key was found."
+        end
+      end
+      cov
+    end
 
     class Coverage
       attr_reader :name, :line_coverages
